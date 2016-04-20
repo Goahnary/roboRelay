36ec46eab96be333e404010c2322cf9574be75e0
jubal-R <jubalrosenbarker@gmail.com>
2016-04-20 11:04:54 -0500

    Updated keycodes

---
+++ js/keycodes.js
@@ -0,0 +1,11 @@
+var keyCodes = {};
+
+    keyCodes[119] = "FORWARD";
+    keyCodes[87] = "FORWARD";
+    keyCodes[97] = "LEFT";
+    keyCodes[65] = "LEFT";
+    keyCodes[100] = "RIGHT";
+    keyCodes[68] = "RIGHT";
+    keyCodes[115] = "BACKWARD";
+    keyCodes[83] = "BACKWARD";
+    keyCodes[32] = "STOP";
