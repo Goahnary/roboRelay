d82a431650d5735e34df8e4399405ce7e40093e5
jubal-R <jubalrosenbarker@gmail.com>
2016-04-20 10:27:27 -0500

    Added sigint to exit cleanly

--- roboRelayServer.py
+++ roboRelayServer.py
@@ -8,6 +8,8 @@
 
 import socket
 import threading
+import signal
+import sys
 from websocket_server import WebsocketServer
 
 clientPort = 8008
@@ -21,6 +23,7 @@ carSock.listen(10)
 threads = []
 inputQueue = []
 carIsConnected = False
+runServer = True
 validInput = ['FORWARD', 'LEFT', 'RIGHT', 'BACKWARD', 'STOP', 'DISCONNECT']
 
 # Semaphores to control access to shared resources inputQueue and carIsConnected
@@ -46,12 +49,14 @@ def communicateWithClient(client, server, msg):
 
 # Websocket to talk to client
 websocket = WebsocketServer(clientPort)
+websocket.set_fn_new_client(onClientConnection)
+websocket.set_fn_client_left(onClientDisconnection)
 websocket.set_fn_message_received(communicateWithClient)
 
 # Handle car communications
 def communicateWithCar():
 
-	while(1):
+	while(runServer):
 		# Wait for car to connect
 		c, addr = carSock.accept()
 		isConnected = True
@@ -63,7 +68,7 @@ def communicateWithCar():
 		print('Car connected')
 
 		# While car is connected: if input queue is not empty, send input to car
-		while(isConnected):
+		while(isConnected and runServer):
 			if(len(inputQueue) > 0):
 				inputSem.acquire()
 				try:
@@ -80,6 +85,26 @@ def communicateWithCar():
 				inputSem.release()
 					
 		c.close()
+
+def signal_handler(signal, frame):
+	print('\nShutting down server..')
+
+	global carIsConnected
+	global runServer
+	carConnectionSem.acquire()
+	runServer = False
+
+	# If car is not connected: connect to self as car then close connection
+	# so thread will continue and check if it needs to stop
+	if(not carIsConnected):
+		s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		s.connect(('127.0.0.1',carPort))
+		s.close()
+
+	carConnectionSem.release()
+
+	sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
 
 # Start car communication thread
